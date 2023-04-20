b31de04fd69204a7faf126ec3a608659e2054632
MaribelSefla <maribelalexandraseflaurquizo@gmail.com>
2023-04-19 19:38:13 -0500

    nuevo
    nueva plantilla

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 /* ===================================================================
- * Hudson 1.0.0 - Main JS
+ * Monica 1.0.0 - Main JS
  *
  * ------------------------------------------------------------------- */
 
 (function(html) {
 
     'use strict';
+
+    const cfg = {
+
+        // MailChimp URL
+        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
+
+    };
 
 
    /* preloader
@@ -32,47 +39,6 @@
         });
 
     }; // end ssPreloader
-
-
-   /* move header
-    * -------------------------------------------------- */
-    const ssMoveHeader = function () {
-
-        const hdr = document.querySelector('.s-header');
-        const hero = document.querySelector('#intro');
-        let triggerHeight;
-
-        if (!(hdr && hero)) return;
-
-        setTimeout(function() {
-            triggerHeight = hero.offsetHeight - 170;
-        }, 300);
-
-        window.addEventListener('scroll', function () {
-
-            let loc = window.scrollY;
-
-            if (loc > triggerHeight) {
-                hdr.classList.add('sticky');
-            } else {
-                hdr.classList.remove('sticky');
-            }
-
-            if (loc > triggerHeight + 20) {
-                hdr.classList.add('offset');
-            } else {
-                hdr.classList.remove('offset');
-            }
-
-            if (loc > triggerHeight + 150) {
-                hdr.classList.add('scrolling');
-            } else {
-                hdr.classList.remove('scrolling');
-            }
-
-        });
-
-    }; // end ssMoveHeader
 
 
    /* mobile menu
@@ -115,70 +81,11 @@
     }; // end ssMobileMenu
 
 
-   /* highlight active menu link on pagescroll
-    * ------------------------------------------------------ */
-    const ssScrollSpy = function() {
-
-        const sections = document.querySelectorAll('.target-section');
-        if (!sections) return;
-
-        // Add an event listener listening for scroll
-        window.addEventListener('scroll', navHighlight);
-
-        function navHighlight() {
-        
-            // Get current scroll position
-            let scrollY = window.pageYOffset;
-        
-            // Loop through sections to get height(including padding and border), 
-            // top and ID values for each
-            sections.forEach(function(current) {
-                const sectionHeight = current.offsetHeight;
-                const sectionTop = current.offsetTop - 50;
-                const sectionId = current.getAttribute('id');
-            
-               /* If our current scroll position enters the space where current section 
-                * on screen is, add .current class to parent element(li) of the thecorresponding 
-                * navigation link, else remove it. To know which link is active, we use 
-                * sectionId variable we are getting while looping through sections as 
-                * an selector
-                */
-                if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.add('current');
-                } else {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.remove('current');
-                }
-            });
-        }
-
-    }; // end ssScrollSpy
-
-
-   /* glightbox
-    * ------------------------------------------------------ */ 
-    const ssGLightbox = function() {
-
-        const lightbox = GLightbox({
-            selector: '.glightbox',
-            zoomable: false,
-            touchNavigation: true,
-            loop: false,
-            autoplayVideos: true,
-            svg: {
-                close: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z"/></svg>',
-                prev: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m9.474 5.209s-4.501 4.505-6.254 6.259c-.147.146-.22.338-.22.53s.073.384.22.53c1.752 1.754 6.252 6.257 6.252 6.257.145.145.336.217.527.217.191-.001.383-.074.53-.221.293-.293.294-.766.004-1.057l-4.976-4.976h14.692c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-14.692l4.978-4.979c.289-.289.287-.761-.006-1.054-.147-.147-.339-.221-.53-.221-.191-.001-.38.071-.525.215z" fill-rule="nonzero"/></svg>',
-                next: '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m14.523 18.787s4.501-4.505 6.255-6.26c.146-.146.219-.338.219-.53s-.073-.383-.219-.53c-1.753-1.754-6.255-6.258-6.255-6.258-.144-.145-.334-.217-.524-.217-.193 0-.385.074-.532.221-.293.292-.295.766-.004 1.056l4.978 4.978h-14.692c-.414 0-.75.336-.75.75s.336.75.75.75h14.692l-4.979 4.979c-.289.289-.286.762.006 1.054.148.148.341.222.533.222.19 0 .378-.072.522-.215z" fill-rule="nonzero"/></svg>'
-            }
-        });        
-
-    } // end ssGLightbox
-
-
    /* swiper
     * ------------------------------------------------------ */ 
     const ssSwiper = function() {
 
-        const testimonialsSwiper = new Swiper('.s-testimonials__slider', {
+        const homeSliderSwiper = new Swiper('.home-slider', {
 
             slidesPerView: 1,
             pagination: {
@@ -194,10 +101,41 @@
                 // when window width is > 800px
                 801: {
                     slidesPerView: 2,
-                    spaceBetween: 50
+                    spaceBetween: 40
                 },
-                // when window width is > 1180px
-                1181: {
+                // when window width is > 1330px
+                1331: {
+                    slidesPerView: 3,
+                    spaceBetween: 48
+                },
+                // when window width is > 1773px
+                1774: {
+                    slidesPerView: 4,
+                    spaceBetween: 48
+                }
+            }
+        });
+
+        const pageSliderSwiper = new Swiper('.page-slider', {
+
+            slidesPerView: 1,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                // when window width is > 400px
+                401: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                // when window width is > 800px
+                801: {
+                    slidesPerView: 2,
+                    spaceBetween: 40
+                },
+                // when window width is > 1240px
+                1241: {
                     slidesPerView: 3,
                     spaceBetween: 48
                 }
@@ -205,6 +143,142 @@
         });
 
     }; // end ssSwiper
+
+
+   /* mailchimp form
+    * ---------------------------------------------------- */ 
+    const ssMailChimpForm = function() {
+
+        const mcForm = document.querySelector('#mc-form');
+
+        if (!mcForm) return;
+
+        // Add novalidate attribute
+        mcForm.setAttribute('novalidate', true);
+
+        // Field validation
+        function hasError(field) {
+
+            // Don't validate submits, buttons, file and reset inputs, and disabled fields
+            if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
+
+            // Get validity
+            let validity = field.validity;
+
+            // If valid, return null
+            if (validity.valid) return;
+
+            // If field is required and empty
+            if (validity.valueMissing) return 'Please enter an email address.';
+
+            // If not the right type
+            if (validity.typeMismatch) {
+                if (field.type === 'email') return 'Please enter a valid email address.';
+            }
+
+            // If pattern doesn't match
+            if (validity.patternMismatch) {
+
+                // If pattern info is included, return custom error
+                if (field.hasAttribute('title')) return field.getAttribute('title');
+
+                // Otherwise, generic error
+                return 'Please match the requested format.';
+            }
+
+            // If all else fails, return a generic catchall error
+            return 'The value you entered for this field is invalid.';
+
+        };
+
+        // Show error message
+        function showError(field, error) {
+
+            // Get field id or name
+            let id = field.id || field.name;
+            if (!id) return;
+
+            let errorMessage = field.form.querySelector('.mc-status');
+
+            // Update error message
+            errorMessage.classList.remove('success-message');
+            errorMessage.classList.add('error-message');
+            errorMessage.innerHTML = error;
+
+        };
+
+        // Display form status (callback function for JSONP)
+        window.displayMailChimpStatus = function (data) {
+
+            // Make sure the data is in the right format and that there's a status container
+            if (!data.result || !data.msg || !mcStatus ) return;
+
+            // Update our status message
+            mcStatus.innerHTML = data.msg;
+
+            // If error, add error class
+            if (data.result === 'error') {
+                mcStatus.classList.remove('success-message');
+                mcStatus.classList.add('error-message');
+                return;
+            }
+
+            // Otherwise, add success class
+            mcStatus.classList.remove('error-message');
+            mcStatus.classList.add('success-message');
+        };
+
+        // Submit the form 
+        function submitMailChimpForm(form) {
+
+            let url = cfg.mailChimpURL;
+            let emailField = form.querySelector('#mce-EMAIL');
+            let serialize = '&' + encodeURIComponent(emailField.name) + '=' + encodeURIComponent(emailField.value);
+
+            if (url == '') return;
+
+            url = url.replace('/post?u=', '/post-json?u=');
+            url += serialize + '&c=displayMailChimpStatus';
+
+            // Create script with url and callback (if specified)
+            var ref = window.document.getElementsByTagName( 'script' )[ 0 ];
+            var script = window.document.createElement( 'script' );
+            script.src = url;
+
+            // Create global variable for the status container
+            window.mcStatus = form.querySelector('.mc-status');
+            window.mcStatus.classList.remove('error-message', 'success-message')
+            window.mcStatus.innerText = 'Submitting...';
+
+            // Insert script tag into the DOM
+            ref.parentNode.insertBefore( script, ref );
+
+            // After the script is loaded (and executed), remove it
+            script.onload = function () {
+                this.remove();
+            };
+
+        };
+
+        // Check email field on submit
+        mcForm.addEventListener('submit', function (event) {
+
+            event.preventDefault();
+
+            let emailField = event.target.querySelector('#mce-EMAIL');
+            let error = hasError(emailField);
+
+            if (error) {
+                showError(emailField, error);
+                emailField.focus();
+                return;
+            }
+
+            submitMailChimpForm(this);
+
+        }, false);
+
+    }; // end ssMailChimpForm
 
 
    /* alert boxes
@@ -301,11 +375,9 @@
     (function ssInit() {
 
         ssPreloader();
-        ssMoveHeader();
         ssMobileMenu();
-        ssScrollSpy();
-        ssGLightbox();
         ssSwiper();
+        ssMailChimpForm();
         ssAlertBoxes();
         ssMoveTo();
 
--- readme.txt
+++ readme.txt
@@ -2,10 +2,11 @@
 
 DESCRIPTION:
 
-HUDSON is a simple and modern free personal website template designed for creatives and 
-professionals who want to showcase their skills and work in an eye-catching way. Its simple yet 
-sophisticated design is perfect for anyone who wants to create a polished and professional online 
-presence without spending a lot of time or money on web development.
+MONICA is a carefully crafted and modern HTML website template designed for freelancers, social 
+media managers, and marketing consultants who help businesses and companies improve their online 
+presence and establish long-lasting relationships with their communities. It is an excellent tool 
+for telling people about your skills and credentials, services, and other offerings, giving them 
+a better idea of who you are.
 
 ==================================================================================================
 
@@ -37,7 +38,7 @@ and not directly to the download zip file.
 Attribution: 
 ------------
 
-You must include a credit link to our website(https://styleshout.com) somewhere on your site. 
+You must include a credit link to our website(https://www.styleshout.com) somewhere on your site. 
 We prefer the footer credit that comes with the template but you are still free to move it 
 somewhere else.
 
@@ -57,7 +58,7 @@ credit removal fee of 10 USD through Paypal. This will allow you to use a single
 template attribution/credit link free on ONE DOMAIN name.
 
 You can send your payments through Paypal to this address: ealigam@gmail.com or
-visit our attribution removal page: https://styleshout.com/attribution-free/ 
+visit our attribution removal page: https://www.styleshout.com/attribution-free/ 
 and click the pay button on the page.
 
 If possible, kindly send us the site's url where the template is being used. 
@@ -69,7 +70,7 @@ Also, keep your Paypal receipt as proof of payment and your good to go.
 
 SUPPORT:
     
-Since HUDSON is distributed for free, support is not offered. HUDSON is coded according 
+Since MONICA is distributed for free, support is not offered. MONICA is coded according 
 to current web standards and we did our best to make the template easy to use and modify.
 If you have minimum web development experience, you can easily modify the template. 
 However, If you're still new to HTML and CSS, I suggest that you visit the 
@@ -86,6 +87,28 @@ javascript in your skill-set, you can also check out these tutorials:
 
 
 
+------------------------------------------------------------------------------------------------------ 
+
+
+USING THE MAILCHIMP FORM:
+
+To use the mailchimp form you need a mailchimp url. To get the mailchimp url, login to 
+your mailchimp account, click the Audience menu and select signup forms. Then choose 
+embedded forms and select unstyled form. In the generated form code get the value of 
+the form ACTION attribute and use it as your mailchimp url. Open the main.js file located
+in the template's "js" folder and look for this line of code: 
+
+const cfg = {
+
+    // MailChimp URL
+    mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
+
+};
+
+
+Replace the value of mailChimpURL with your mailchimp url.
+
+
 
 -------------------------------------------------------------------------------------------------------
 
@@ -93,7 +116,7 @@ javascript in your skill-set, you can also check out these tutorials:
 GET THE LATEST VERSION:
 
 We update our templates on a regular basis so to make sure that you have the latest version, 
-always download the template files directly on our website(https://styleshout.com/)
+always download the template files directly on our website(https://www.styleshout.com/)
 
 
 
@@ -105,12 +128,12 @@ SOURCES AND CREDITS:
 I've used the following resources as listed.
 
 Fonts:
- - Castoro Font (https://fonts.google.com/specimen/Castoro)
- - Public Sans Font (https://fonts.google.com/specimen/Public+Sans)
+ - DM Serif Display Font (https://fonts.google.com/specimen/DM+Serif+Display)
+ - Roboto Flex Font (https://fonts.google.com/specimen/Roboto+Flex)
 
 Icons:
  - Boxicons (https://boxicons.com/)
- - Iconmonstr (https://iconmonstr.com/)
+ - Atomicons (https://atomicons.com/)
 
 Stock Photos and Graphics:
  - Unsplash.com (https://unsplash.com/)
@@ -119,7 +142,6 @@ Javascript Files:
  - MoveTo.js (https://github.com/hsnaydd/moveTo)
  - Prism.js (https://prismjs.com/)
  - Swiper JS (https://swiperjs.com/)
- - GLightbox (https://biati-digital.github.io/glightbox/)
 
 -------------------------------------------------------------------------------------------------------
 
